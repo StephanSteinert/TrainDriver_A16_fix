89f7d1671fb3682f664cdb270ad819c690586f4b
houseofsteinert <14843079+StephanSteinert@users.noreply.github.com>
2019-08-24 21:02:29 +0200

    0.17.2

--- control.lua
+++ control.lua
@@ -1,58 +1,39 @@
 require "util"
 require "utils.set"
 require "utils.train"
-require "config"
-
-function onInit()
-    global.directions_active = global.directions_active or {}
-end
-
-function onConfigurationChanged(data)
-    global.directions_active = global.directions_active or {}
-end
 
 function init_player_table(player_index)
-    if global.active_texts == nil then
-        global.active_texts = {}
-    end
+    global.active_texts[player_index] = global.active_texts[player_index] or {}
 
-    if global.active_texts[player_index] == nil then
-        global.active_texts[player_index] = {}
+    if global.directions_active[player_index] == nil then
+        global.directions_active[player_index] = true
     end
 end
 
+-- Create flying text for given player
 function make_flying_text(text, pos, player_index)
-    local text_color = nil
-    
-    if cfg_directions_color == nil then
-        text_color = game.players[player_index].color
-    else
-        text_color = cfg_directions_color
-    end
+    local player = game.players[player_index]
+    local fly    = player.surface.create_entity{
+        name     = "flying-text",
+        text     = text,
+        color    = player.color,
+        position = pos
+    }
+    fly.active   = false    
 
-    local fly = game.players[player_index].surface.create_entity{
-        name="flying-text",
-        text=text,
-        color=text_color,
-        position=pos}
-    
-    fly.active = false
-    
-    init_player_table(player_index)
     table.insert(global.active_texts[player_index], fly)
     
     return fly
 end
 
+-- destroy all flying texts for given player
 function erase_current_marks(player_index)
-    init_player_table(player_index)
-
-    for i = #global.active_texts[player_index], 1, -1 do
-        if global.active_texts[player_index][i] and global.active_texts[player_index][i].valid then
-            global.active_texts[player_index][i].destroy()
+    for active_text_id, active_text in pairs(global.active_texts[player_index]) do
+        if active_text.valid then
+            active_text.destroy()
         end
         
-        table.remove(global.active_texts[player_index], i)
+        table.remove(global.active_texts[player_index], active_text_id)
     end
 end
 
@@ -72,31 +53,31 @@ function get_carriage_type(train, carriage)
     return "carriage"
 end
 
-function find_rail(player_index, player, train, front_rail, fwd_dir, depth)
+function find_rail(player, train, front_rail, fwd_dir, depth)
     if depth > 10 then
         -- too far away
-        return nil
+        return
     end
 
-    local fwd_straight = front_rail.get_connected_rail{rail_direction=fwd_dir, rail_connection_direction=defines.rail_connection_direction.straight}
-    local fwd_left = front_rail.get_connected_rail{rail_direction=fwd_dir, rail_connection_direction=defines.rail_connection_direction.left}
-    local fwd_right = front_rail.get_connected_rail{rail_direction=fwd_dir, rail_connection_direction=defines.rail_connection_direction.right}
-    
+    local player_index  = player.index
+    local fwd_straight  = front_rail.get_connected_rail{rail_direction = fwd_dir, rail_connection_direction = defines.rail_connection_direction.straight}
+    local fwd_left      = front_rail.get_connected_rail{rail_direction = fwd_dir, rail_connection_direction = defines.rail_connection_direction.left}
+    local fwd_right     = front_rail.get_connected_rail{rail_direction = fwd_dir, rail_connection_direction = defines.rail_connection_direction.right}
     local present_count = 0
-    local present_val = nil
+    local present_val   = nil
     
     if fwd_straight then
-        present_val = fwd_straight
+        present_val   = fwd_straight
         present_count = present_count + 1
     end
     
     if fwd_left then
-        present_val = fwd_left
+        present_val   = fwd_left
         present_count = present_count + 1
     end
     
     if fwd_right then
-        present_val = fwd_right
+        present_val   = fwd_right
         present_count = present_count + 1
     end
     
@@ -119,88 +100,109 @@ function find_rail(player_index, player, train, front_rail, fwd_dir, depth)
             end
         end
         
-        return find_rail(player_index, player, train, present_val, new_fwd_dir, depth+1)
+        return find_rail(player, train, present_val, new_fwd_dir, depth + 1)
     end
     
-    if fwd_straight and cfg_directions_show_straight then
-        make_flying_text(cfg_directions_straight, fwd_straight.position, player_index)
+    if fwd_straight and player.mod_settings["train_driver_show_straight"].value then
+        make_flying_text({"message.train_driver_straight"}, fwd_straight.position, player_index)
     end
     
     local carriage_type = get_carriage_type(train, player.vehicle)
 
     if fwd_left then
-        if (train.speed > 0 and carriage_type == "front_mover") or (train.speed < 0 and carriage_type == "back_mover") or carriage_type == "carriage" then
-            make_flying_text(cfg_directions_left, fwd_left.position, player_index)
+        if (train.speed >= 0 and carriage_type == "front_mover") or (train.speed <= 0 and carriage_type == "back_mover") or carriage_type == "carriage" then
+            make_flying_text({"message.train_driver_left"}, fwd_left.position, player_index)
         else
-            make_flying_text(cfg_directions_right, fwd_left.position, player_index)
+            make_flying_text({"message.train_driver_right"}, fwd_left.position, player_index)
         end
     end
 
     if fwd_right then
-        if (train.speed > 0 and carriage_type == "front_mover") or (train.speed < 0 and carriage_type == "back_mover") or carriage_type == "carriage" then
-            make_flying_text(cfg_directions_right, fwd_right.position, player_index)
+        if (train.speed >= 0 and carriage_type == "front_mover") or (train.speed <= 0 and carriage_type == "back_mover") or carriage_type == "carriage" then
+            make_flying_text({"message.train_driver_right"}, fwd_right.position, player_index)
         else
-            make_flying_text(cfg_directions_left, fwd_right.position, player_index)
+            make_flying_text({"message.train_driver_left"}, fwd_right.position, player_index)
         end
     end
-    
-    return true
 end
 
-function print_directions(player_index, player)
-    erase_current_marks(player_index)
+function print_directions()
+    for _, player in pairs(game.players) do
+        local player_index = player.index
 
-    if player.vehicle == nil or (player.vehicle.type ~= "locomotive" and player.vehicle.type ~= "cargo-wagon") or player.vehicle.train == nil or not player.vehicle.train.manual_mode then
-        return nil
-    end
-    
-    local train = player.vehicle.train
-    
-    local front_rail = nil
-    local fwd_dir = nil
-    
-    if train.speed > 0 then
-        front_rail = train.front_rail
-        fwd_dir = train.rail_direction_from_front_rail
-    elseif train.speed < 0 then
-        front_rail = train.back_rail
-        fwd_dir = train.rail_direction_from_back_rail
+        init_player_table(player_index)
 
-        if fwd_dir == defines.rail_direction.front then
-            fwd_dir = defines.rail_direction.back
-        else
-            fwd_dir = defines.rail_direction.front
+        if global.directions_active[player_index] == false then 
+            erase_current_marks(player_index)
+            return 
         end
-    else
-        return nil
-    end
+        
+        if player.vehicle == nil or 
+           (player.vehicle.type ~= "locomotive" and player.vehicle.type ~= "cargo-wagon") or 
+           player.vehicle.train == nil or 
+           not player.vehicle.train.manual_mode then
+            erase_current_marks(player_index)
+            return
+        end
 
-    return find_rail(player_index, player, train, front_rail, fwd_dir, 0)
-end
+        local train      = player.vehicle.train
+        local front_rail = nil
+        local fwd_dir    = nil
 
+        erase_current_marks(player_index)
+        if train.speed > 0 then
+            find_rail(player, train, train.front_rail, train.rail_direction_from_front_rail, 0)
+        end
 
-script.on_init(onInit)
-script.on_configuration_changed(onConfigurationChanged)
-
-script.on_event(defines.events.on_tick, function(event)
-    if event.tick % 10 == 0 then
-        global.directions_active = global.directions_active or {}
+        if train.speed < 0 then
+            fwd_dir = train.rail_direction_from_back_rail
     
-        for i = 1, #game.players do
-            if global.directions_active[i] then
-                print_directions(i, game.players[i])
+            if fwd_dir == defines.rail_direction.front then
+                fwd_dir = defines.rail_direction.back
+            else
+                fwd_dir = defines.rail_direction.front
             end
+
+            find_rail(player, train, train.back_rail, fwd_dir, 0)
+        end
+
+        if train.speed == 0 then
+            find_rail(player, train, train.front_rail, train.rail_direction_from_front_rail, 0)
+
+            fwd_dir = train.rail_direction_from_back_rail
+            if fwd_dir == defines.rail_direction.front then
+                fwd_dir = defines.rail_direction.back
+            else
+                fwd_dir = defines.rail_direction.front
+            end
+            find_rail(player, train, train.back_rail, fwd_dir, 0)
         end
     end
+end
+
+function init()
+    global.active_texts      = global.active_texts or {}
+    global.directions_active = global.directions_active or {}
+end
+
+script.on_init(init)
+script.on_configuration_changed(init)
+
+script.on_nth_tick(10, function(event)
+    print_directions()
 end)
 
+-- Toogle display of directions with key combo
 script.on_event("train_driver_directions", function(event)
-    if global.directions_active[event.player_index] then
-        global.directions_active[event.player_index] = false
-        erase_current_marks(event.player_index)
-        game.players[event.player_index].print("Train maneuver directions disabled.")
+    local player_index = event.player_index
+
+    if global.directions_active[player_index] == true then
+        global.directions_active[player_index] = false
+        game.players[player_index].print({"message.train_driver_disabled"})
+
+        erase_current_marks(player_index)
     else
-        global.directions_active[event.player_index] = true
-        game.players[event.player_index].print("Train maneuver directions enabled.")
+        global.directions_active[player_index] = true
+        game.players[player_index].print({"message.train_driver_enabled"})
     end
 end)
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend{
+	{
+		type = "bool-setting",
+		name = "train_driver_show_straight",
+		setting_type = "runtime-per-user",
+		order = "a",
+		default_value = true
+	}
+}
